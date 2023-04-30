9f6823a9d8989d0f69f49bac6e7f60041dee006d
Stefan Hall <stefanhall@SHAL003975.attlocal.net>
2023-04-29 23:15:41 -0400

    seperating modules

---
+++ frontend.py
@@ -0,0 +1,42 @@
+from user import PlayerChampionships
+
+# frontent.py will be the file that you would be creating your functions to render HTML and pass in context variables, 
+# showing database data in the UI and interacting with the database through the framework of your choosing.
+
+# Instantiating PlayerChampionships().
+user = PlayerChampionships()
+
+player_name = "Michael"
+player_rings = "6 Championships"
+
+# Adding Player.
+user.add_user(player_name, player_rings)
+
+player2_name = "Kobe"
+player2_rings = "5 Championships"
+
+# Adding Player.
+user.add_user(player2_name, player2_rings)
+
+player3_name = "Lebron"
+player3_rings = "4 Championships"
+
+# Adding Player.
+user.add_user(player3_name, player3_rings)
+
+player4_name = "Magic"
+player4_rings = "4 Championships"
+
+# Adding Player.
+user.add_user(player4_name, player4_rings)
+
+player4_rings = "5 Championships"
+
+# Change player4_rings from "4 Championships" -> "5 Championships".
+user.patch_user(player4_name, player4_rings)
+
+player5_name = "Kareem"
+playter5_rings = "6 Championships"
+
+# Changing the entire player 4 -> player 5.
+user.put_user(player4_name, player5_name, playter5_rings)
